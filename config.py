2ad29a939a263b03d3ec7fe42276ac432b622db6
Samuel Diaz <sdman135@gmail.com>
2020-07-04 20:56:36 -0400

    edit

--- config.py
+++
@@ -1,6 +0,0 @@
-
-consumer_key = 'qkkSeWr8smUj1Z6n9hI6C8vo1' 
-consumer_secret = '35NSNkmqulwOrTOrfchixtdRsdnUvEfPMdzNUt57u6jhce1Bg1'
-
-access_token = '1237048771919446018-u1eel3vsCrM4hyPXmPM0N3CXaRRySY'
-access_token_secret = 'rWwkegV1KzLaqt1c8JbSaXnEOzLlfhYGwZoGlEV42IcA5'
